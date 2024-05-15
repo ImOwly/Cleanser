46a1fe1c0555dab54cb5cc359aa61dd358700f9c
Cleon <chengcleon@hotmail.com>
2024-05-14 19:54:46 -0700

    added gun selection menu

--- gun/gun_manager.gd
+++ gun/gun_manager.gd
@@ -15,41 +15,54 @@ var firing_angle_variation = 0.10472
 var bullet_speed_variation = 100
 
 enum Gun_Types {shotgun, rifle, spraygun}
-var current_gun = Gun_Types.spraygun
+var selected_gun = Gun_Types.spraygun
+var current_gun
+var gen_gun = true
 
 @onready var spray = preload("res://projectiles/spray.tscn")
 @onready var bullet_rifle = preload("res://projectiles/bullet_rifle.tscn")
 @onready var bullet_shotgun = preload("res://projectiles/bullet_shotgun.tscn")
-@onready var cursor = preload("res://assets/sprites/spray.png")
+@onready var shotgun = preload("res://gun/shotgun.tscn")
+@onready var rifle = preload("res://gun/rifle.tscn")
+@onready var spraygun = preload("res://gun/spraygun.tscn")
 
 func _ready():
+	current_gun = spraygun.instantiate()
 	can_fire = true
+	$SprayButton.visible = false
+	$ShotgunButton.visible = false
+	$RifleButton.visible = false
 
 func _input(event):
 	if (event is InputEventMouseButton and event.pressed):
 		if event.button_index == MOUSE_BUTTON_RIGHT:
-			print(current_gun)
-			current_gun = (current_gun + 1) % 3
+			toggle_gun_menu()
 
 func _process(delta):
-	look_at(get_global_mouse_position())
+	generate_gun()
+	current_gun.position = self.get_global_position()
+	current_gun.look_at(get_global_mouse_position())
 	
 	#When left-mouse is held down
 	if Input.is_mouse_button_pressed(1) and can_fire:
-		#$GPUParticles2D.emitting = true
-		match current_gun:
+		match selected_gun:
 			Gun_Types.spraygun:
-				spray_gun()
+				fire_spray_gun()
 			Gun_Types.shotgun:
-				shotgun()
+				fire_shotgun()
 			Gun_Types.rifle:
-				rifle()
+				fire_rifle()
 	
-func rifle():
+func generate_gun():
+	if gen_gun:
+		gen_gun = false
+		get_tree().get_root().add_child(current_gun)
+	
+func fire_rifle():
 	fire_bullet(0, bullet_speed*3, bullet_rifle)
 	fire_delay(fire_rate_rifle)
 	
-func shotgun():
+func fire_shotgun():
 	fire_bullet(0, bullet_speed*2, bullet_shotgun)
 	var spread = 0.105
 	for i in range(0, 2):
@@ -57,7 +70,7 @@ func shotgun():
 		fire_bullet(spread, bullet_speed*2, bullet_shotgun)
 	fire_delay(fire_rate_shotgun)
 	
-func spray_gun():
+func fire_spray_gun():
 	fire_multiple_bullets(spray, 2)
 	await get_tree().create_timer(.02).timeout
 	fire_multiple_bullets(spray, 1)
@@ -74,12 +87,43 @@ func fire_multiple_bullets(bullet, x):
 	
 func fire_bullet(offset, velocity, bullet):
 	var bullet_instance = bullet.instantiate()
-	bullet_instance.position = $GunBarrel.get_global_position()
-	bullet_instance.rotation_degrees = rotation_degrees
-	bullet_instance.apply_impulse(Vector2(velocity, 0).rotated(rotation-offset))
+	bullet_instance.position = current_gun.find_child("GunBarrel").get_global_position()
+	bullet_instance.rotation_degrees = current_gun.rotation_degrees
+	bullet_instance.apply_impulse(Vector2(velocity, 0).rotated(current_gun.rotation-offset))
 	get_tree().get_root().add_child(bullet_instance)
 	
 func fire_delay(time):
 	can_fire = false
 	await get_tree().create_timer(time).timeout
 	can_fire = true
+
+func toggle_gun_menu():
+	if (!$SprayButton.visible or !$ShotgunButton.visible or !$RifleButton.visible):
+		$SprayButton.visible = true
+		$ShotgunButton.visible = true
+		$RifleButton.visible = true
+	else:
+		$SprayButton.visible = false
+		$ShotgunButton.visible = false
+		$RifleButton.visible = false
+				
+func _on_spray_button_button_up():
+	selected_gun = Gun_Types.spraygun
+	current_gun.queue_free()
+	current_gun = spraygun.instantiate()
+	get_tree().get_root().add_child(current_gun)
+	toggle_gun_menu()
+
+func _on_shotgun_button_button_up():
+	selected_gun = Gun_Types.shotgun
+	current_gun.queue_free()
+	current_gun = shotgun.instantiate()
+	get_tree().get_root().add_child(current_gun)
+	toggle_gun_menu()
+
+func _on_rifle_button_button_up():
+	selected_gun = Gun_Types.rifle
+	current_gun.queue_free()
+	current_gun = rifle.instantiate()
+	get_tree().get_root().add_child(current_gun)
+	toggle_gun_menu()
--- projectiles/spray.gd
+++ projectiles/spray.gd
@@ -1,5 +1,5 @@
 extends RigidBody2D
-var lifetime = 10
+var lifetime = .7
 var colors = [Color(0.0, 255.0, 255.0, 1.0), 
 			Color("06c3dd", 1.0), 
 			Color("24909f", 1.0),
