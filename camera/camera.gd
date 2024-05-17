338336ba3ad337e532c11562040c9b97123ff661
Cleon <chengcleon@hotmail.com>
2024-05-17 12:16:42 -0700

    updating sprites and camera

--- camera/camera.gd
+++ camera/camera.gd
@@ -4,4 +4,4 @@ extends Camera2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = player.position
+	self.position = Vector2(player.position.x, self.position.y)
