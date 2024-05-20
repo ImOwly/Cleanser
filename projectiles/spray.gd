4540a4259ce93a08053c08c7c3bb0edaf8790dd1
Matt <mattz3044@outlook.com>
2024-05-20 01:29:15 -0700

    sprite and tilemap changes

--- projectiles/spray.gd
+++ projectiles/spray.gd
@@ -16,9 +16,61 @@ func _physics_process(delta):
 	if tile_map != null:
 		var tilePos = tile_map.local_to_map(self.global_position)
 		var data = tile_map.get_cell_tile_data(0, tilePos)
+		var wall_data = tile_map.get_cell_tile_data(1, tilePos)
+		var current_wall_Atlas_Id = tile_map.get_cell_atlas_coords(1, tilePos)
+		if(wall_data):
+			match current_wall_Atlas_Id:
+				Vector2i(13,1):
+					var atlasCord: Vector2i = Vector2i(13,0)
+					tile_map.set_cell(1, tilePos, 0, atlasCord)
+				Vector2i(14,1):
+					var atlasCord: Vector2i = Vector2i(14,0)
+					tile_map.set_cell(1, tilePos, 0, atlasCord)
+				Vector2i(15,1):
+					var atlasCord: Vector2i = Vector2i(15,0)
+					tile_map.set_cell(1, tilePos, 0, atlasCord)
 		var current_Atlas_Id = tile_map.get_cell_atlas_coords(0, tilePos)
 		if(data):
 			match current_Atlas_Id:
+				Vector2i(3,7):
+					var atlasCord : Vector2i = Vector2i(0, 7)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(4,7):
+					var atlasCord : Vector2i = Vector2i(1, 7)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(5,7):
+					var atlasCord : Vector2i = Vector2i(2, 7)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(3,8):
+					var atlasCord : Vector2i = Vector2i(0, 8)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(5,8):
+					var atlasCord : Vector2i = Vector2i(2, 8)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(3,9):
+					var atlasCord : Vector2i = Vector2i(0, 9)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(4,9):
+					var atlasCord : Vector2i = Vector2i(1, 9)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(5,9):
+					var atlasCord : Vector2i = Vector2i(2, 9)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(4,11):
+					var atlasCord : Vector2i = Vector2i(0, 11)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(6,11):
+					var atlasCord : Vector2i = Vector2i(2, 11)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(5,12):
+					var atlasCord : Vector2i = Vector2i(1, 12)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(6,13):
+					var atlasCord : Vector2i = Vector2i(2, 13)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(4,13):
+					var atlasCord : Vector2i = Vector2i(0, 13)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
 				Vector2i(0,1):
 					var atlasCord : Vector2i = Vector2i(0, 0)
 					tile_map.set_cell(0, tilePos, 0, atlasCord)
@@ -40,3 +92,22 @@ func _physics_process(delta):
 				Vector2i(8,4):
 					var atlasCord : Vector2i = Vector2i(8, 0)
 					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(11,7):
+					var atlasCord : Vector2i = Vector2i(11, 6)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(12,7):
+					var atlasCord : Vector2i = Vector2i(12, 6)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(13,7):
+					var atlasCord : Vector2i = Vector2i(13, 6)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(24,2):
+					var atlasCord : Vector2i = Vector2i(17, 2)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(24,7):
+					var atlasCord : Vector2i = Vector2i(17, 7)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				Vector2i(24,14):
+					var atlasCord : Vector2i = Vector2i(17, 14)
+					tile_map.set_cell(0, tilePos, 0, atlasCord)
+				
